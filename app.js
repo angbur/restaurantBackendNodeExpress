898eb8f4ecc7d3baf975e6f24bd065b83dac3398
angbur <ag.grala@gmail.com>
2023-01-02 20:40:55 +0100

    add cors

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const pizzaRouter = require('./api/routes/pizzaRoutes')
+const pizzaRouter = require('./api/routes/pizzaRoutes');
+const cors = require('cors');
 
 app.use((req, res, next) => {
   const error = new Error('This URL does not exist')
   next()
-})
+});
+const corsOptions = {
+  exposedHeaders: ["Accept","Accept-Language","Content-Language", "X-Requested-With", "Authorization", "Content-Type"],
+  allowedHeaders: ["*","Accept","Accept-Language","Content-Language", "X-Requested-With", "Authorization", "Content-Type"],
+  origin: '*'
+};
+app.use('/',cors(corsOptions));
 
 dotenv.config()
 
